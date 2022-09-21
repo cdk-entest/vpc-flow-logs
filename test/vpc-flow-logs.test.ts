5ab9e412b78a5e5b99c9fa21bfb137e37f11288b
entest <hai@entest.io>
2022-09-21 06:03:36 +0000

    Initial commit

---
+++ test/vpc-flow-logs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VpcFlowLogs from '../lib/vpc-flow-logs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vpc-flow-logs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VpcFlowLogs.VpcFlowLogsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
