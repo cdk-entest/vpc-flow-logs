4468e1cb23970e4c0d6eb38b8736d67d34771ad3
entest <hai@entest.io>
2022-09-22 03:59:37 +0000

    first

--- bin/vpc-flow-logs.ts
+++ bin/vpc-flow-logs.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { VpcFlowLogsStack } from '../lib/vpc-flow-logs-stack';
+import * as cdk from "aws-cdk-lib";
+import { FlowLogStack, Ec2Stack } from "../lib/vpc-flow-logs-stack";
+import CONFIG from "./../config.json";
 
 const app = new cdk.App();
-new VpcFlowLogsStack(app, 'VpcFlowLogsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new Ec2Stack(app, "Ec2Stack", {
+  vpcId: CONFIG.VPC_ID,
+  vpcName: CONFIG.VPC_NAME,
+  env: {
+    region: "us-east-1",
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+});
+new FlowLogStack(app, "FlowLogStack", {
+  vpcId: CONFIG.VPC_ID,
+  vpcName: CONFIG.VPC_NAME,
+  interfaceId: "eni-0fafc042c5f32c0fe",
+  env: {
+    region: "us-east-1",
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
 });
--- lib/vpc-flow-logs-stack.ts
+++ lib/vpc-flow-logs-stack.ts
@@ -1,16 +1,176 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Stack,
+  StackProps,
+  aws_ec2,
+  aws_iam,
+  aws_logs,
+  RemovalPolicy,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Effect } from "aws-cdk-lib/aws-iam";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
 
-export class VpcFlowLogsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface VpcFlowLogsProps extends StackProps {
+  vpcId: string;
+  vpcName: string;
+  interfaceId: string;
+}
+
+interface Ec2StackProps extends StackProps {
+  vpcId: string;
+  vpcName: string;
+}
+
+export class Ec2Stack extends Stack {
+  constructor(scope: Construct, id: string, props: Ec2StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // lookup existing vpc
+    const vpc = aws_ec2.Vpc.fromLookup(this, "existingVpc", {
+      vpcId: props.vpcId,
+      vpcName: props.vpcName,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcFlowLogsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const role = new aws_iam.Role(this, "RoleForEc2AccessSSM", {
+      assumedBy: new aws_iam.ServicePrincipal("ec2.amazonaws.com"),
+      roleName: "RoleForEc2AccessSSM",
+    });
+
+    role.addManagedPolicy(
+      aws_iam.ManagedPolicy.fromManagedPolicyArn(
+        this,
+        "PolicySSMAccessS3",
+        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      )
+    );
+
+    role.addToPolicy(
+      new aws_iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: ["*"],
+        actions: ["s3:*"],
+      })
+    );
+
+    new aws_ec2.Instance(this, "Ec2Test", {
+      instanceName: "Ec2Test",
+      vpc: vpc,
+      role: role,
+      vpcSubnets: {
+        subnetType: aws_ec2.SubnetType.PUBLIC,
+      },
+      instanceType: aws_ec2.InstanceType.of(
+        aws_ec2.InstanceClass.T3,
+        aws_ec2.InstanceSize.SMALL
+      ),
+      machineImage: new aws_ec2.AmazonLinuxImage({
+        generation: aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        edition: aws_ec2.AmazonLinuxEdition.STANDARD,
+      }),
+      allowAllOutbound: true,
+    });
+  }
+}
+
+export class FlowLogStack extends Stack {
+  constructor(scope: Construct, id: string, props: VpcFlowLogsProps) {
+    super(scope, id, props);
+
+    // cloudwatch log group
+    const logGroup = new aws_logs.LogGroup(this, "Ec2FlowLogLogGroup", {
+      logGroupName: "Ec2FlowLogLogGroup",
+      retention: RetentionDays.ONE_DAY,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // filter accept request
+    const filterAccept = new aws_logs.MetricFilter(
+      this,
+      "FilterAcceptFlowLog",
+      {
+        logGroup,
+        metricNamespace: "FlowLogFilter",
+        metricName: "Accept",
+        filterPattern: aws_logs.FilterPattern.spaceDelimited(
+          "version",
+          "accountid",
+          "interfaceid",
+          "srcaddr",
+          "dstaddr",
+          "srcport",
+          "dstport",
+          "protocol",
+          "packets",
+          "bytes",
+          "start",
+          "end",
+          "action",
+          "logstatus"
+        ).whereString("action", "=", "ACCEPT"),
+        metricValue: "$bytes",
+        defaultValue: 0,
+      }
+    );
+    const metricAccept = filterAccept.metric();
+
+    // filter accept request
+    const filterReject = new aws_logs.MetricFilter(
+      this,
+      "FilterRejectFlowLog",
+      {
+        logGroup,
+        metricNamespace: "FlowLogFilter",
+        metricName: "Reject",
+        filterPattern: aws_logs.FilterPattern.spaceDelimited(
+          "version",
+          "accountid",
+          "interfaceid",
+          "srcaddr",
+          "dstaddr",
+          "srcport",
+          "dstport",
+          "protocol",
+          "packets",
+          "bytes",
+          "start",
+          "end",
+          "action",
+          "logstatus"
+        ).whereString("action", "=", "REJECT"),
+        metricValue: "$bytes",
+        defaultValue: 0,
+      }
+    );
+    const metricReject = filterReject.metric();
+
+    // alarm  actions based on metrics
+
+    // role for flowlogs to publish to log group
+    const roleForFlowLog = new aws_iam.Role(
+      this,
+      "RoleForFlowLogPublishToLogGroup",
+      {
+        roleName: "RoleForFlowLogPublishToLogGroup",
+        assumedBy: new aws_iam.ServicePrincipal("vpc-flow-logs.amazonaws.com"),
+      }
+    );
+
+    roleForFlowLog.addToPolicy(
+      new aws_iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [logGroup.logGroupArn],
+        actions: ["logs:*"],
+      })
+    );
+
+    // flowlogs to monitor the ec2 network interface
+    new aws_ec2.CfnFlowLog(this, "Ec2FlowLog", {
+      resourceId: props.interfaceId,
+      resourceType: "NetworkInterface",
+      trafficType: "ALL",
+      deliverLogsPermissionArn: roleForFlowLog.roleArn,
+      logGroupName: logGroup.logGroupName,
+      maxAggregationInterval: 60,
+    });
   }
 }
